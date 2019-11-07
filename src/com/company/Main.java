eb6b0baa8a8c079b39e83163283b49944995fad2
ekaterina163 <katerina16393@mail.ru>
2019-11-07 19:54:57 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int currentN = max; currentN <= max; currentN -= step ){
+
+    }
+    }
+    static void printMultipleTable(){
+        int i = 1;
+        int j = 1;
+
+        while (i <= 10) {
+            while (j<=10) {
+                System.out.println((i*j) + "");
+
+            }
+        }
+    }
+}
